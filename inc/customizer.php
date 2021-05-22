bda3e76e05d1669f0655b7923259d7cb50f0bf7f
IamCooller <45273146+IamCooller@users.noreply.github.com>
2021-05-22 19:19:56 +0300

    Add files via upload

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * stop_addiction Theme Customizer
+ *
+ * @package stop_addiction
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function stop_addiction_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'stop_addiction_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'stop_addiction_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'stop_addiction_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function stop_addiction_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function stop_addiction_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function stop_addiction_customize_preview_js() {
+	wp_enqueue_script( 'stop_addiction-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'stop_addiction_customize_preview_js' );
